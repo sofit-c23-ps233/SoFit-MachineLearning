becfaf1e825db6642b6761c38d4c1fc16555c4e4
mamang69 <elditamvan15@gmail.com>
2023-06-05 21:54:00 +0700

    Convert Model to API

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-import * as tf from '@tensorflow/tfjs';
-const model = await tf.loadLayersModel("https://raw.githubusercontent.com/sofit-c23-ps233/SoFit-MachineLearning/main/model.json");
-const example = tf.tensor2d([[0, 170, 102]]);
-const prediction = model.predict(example).argMax(1).dataSync()[0];
-console.log(prediction);
+const express = require('express');
+const tf = require('@tensorflow/tfjs');
+const app = express();
+
+app.use(express.json());
+
+const categoryleveling = ['Extremely Weak', 'Weak', 'Ideal', 'Overweight', 'Obesity', 'Extreme Obesity'];
+
+app.post('/predict', async (req, res) => {
+  const model = await tf.loadLayersModel('https://raw.githubusercontent.com/sofit-c23-ps233/SoFit-MachineLearning/main/model.json');
+
+  let data = {
+    gender: Number(req.body.gender),
+    height: Number(req.body.height),
+    weight: Number(req.body.weight),
+  };
+
+  const example = tf.tensor2d([[data.gender, data.height, data.weight]]);
+  const prediction = model.predict(example).argMax(1).dataSync()[0];
+  const category = categoryleveling[prediction];
+  res.json({ category });
+});
+
+app.listen(3000, () => {
+  console.log('API run on port 3000');
+});
